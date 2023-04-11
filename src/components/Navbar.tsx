cec06d9b5dc3417b4c8ca70f6d798e857d90f0b6
Reza Hussain <alirezaa08@gmail.com>
2023-04-12 01:59:10 +0530

    Added Sidebar, Navbar and other components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { MdAdd } from "react-icons/md";
+
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
-          <Image className="cursor-pointer" src={Logo} alt="logo" fill={true} />
+        <div className="w-[100px] md:w-[130px] md:h-[30px] h-[38px]"> 
+          <Image src={Logo} alt="Logo" width={100} height={100} className="cursor-pointer" />
         </div>
       </Link>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,16 +15,18 @@ export default function App({ Component, pageProps }: AppProps) {
   if (isSSR) return null;
 
   return (
-    <div>
-      <Navbar />
-      <div className="flex gap-6 md:gap-20 ">
-        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          <Sidebar />
-        </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-          <Component {...pageProps} />
+    <GoogleOAuthProvider clientId="fehfeh">
+      <div>
+        <Navbar />
+        <div className="flex gap-6 md:gap-20 ">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
